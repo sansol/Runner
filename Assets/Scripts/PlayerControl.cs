67c2956ae9dfc96d0c0604be1353c37f1c5b0296
sansol <sansol@gmail.com>
2015-02-07 17:32:17 +0100

    skeleton for the game

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float speed;
+
+	void Update()
+	{
+/*		if (Input.GetButton ("Fire1") && Time.time > nextFire) 
+		{
+			//actions taken by the player like jumping or shooting (if any)
+		}*/
+	}
+	
+	void FixedUpdate () 
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rigidbody.velocity = movement*speed;
+	}
+}
