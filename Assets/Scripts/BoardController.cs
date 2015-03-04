cbf21078f2d9a19f2d1fdfffc451bc4185fe455d
sansol <sansol@gmail.com>
2015-03-04 19:16:55 +0100

    board controller and game controller
    game controller ready to be unique instance between scenes
    save and load game data functionality, but not used
    board controller loads new board from prefab based on hardcoded path

---
+++ Assets/Scripts/BoardController.cs
@@ -0,0 +1,251 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class BoardController : MonoBehaviour {
+
+    private Path currentPath = new Path();
+
+    // object to hold the whole path
+    public GameObject boardHolder;
+
+    // to be able to state the prefabs
+    public GameObject tileVertical;
+    public GameObject tileCornerSE;
+
+    private Dictionary<TileType, TileConfiguration> prefabTiles = new Dictionary<TileType, TileConfiguration>();
+
+    public void BoardSetup()
+    {
+        ConfigurePrefabs();
+        TileType[] newPathTileTypes = {
+            TileType.Vertical,
+            TileType.CornerSW,
+            TileType.CornerSE,
+            TileType.Vertical,
+            TileType.CornerNE,
+
+            TileType.Horizontal,
+            TileType.CornerNW,
+            TileType.CornerSE,
+            TileType.Horizontal,
+            TileType.CornerNW,
+
+            TileType.Vertical,
+            TileType.CornerSW,
+            TileType.CornerSE,
+            TileType.CornerNW,
+            TileType.CornerNE,
+
+            TileType.CornerSW,
+            TileType.Horizontal,
+            TileType.Horizontal
+        }; 
+        GenerateNewPathFromTileTypes(newPathTileTypes);
+        LoadTrack(new Vector3(0.0f, 0.0f, 0.0f));
+    }
+
+    void ConfigurePrefabs()
+    {
+        // Set reused prefabs rotation
+        prefabTiles.Add(TileType.Vertical, new TileConfiguration(TileType.Vertical, tileVertical, new Vector3(0.0f, 0.0f, 0.0f)));
+        prefabTiles.Add(TileType.Horizontal, new TileConfiguration(TileType.Horizontal, tileVertical, new Vector3(0.0f, 90.0f, 0.0f)));
+        prefabTiles.Add(TileType.CornerSE, new TileConfiguration(TileType.CornerSE, tileCornerSE, new Vector3(0.0f, 0.0f, 0.0f)));
+        prefabTiles.Add(TileType.CornerSW, new TileConfiguration(TileType.CornerSW, tileCornerSE, new Vector3(0.0f, 90.0f, 0.0f)));
+        prefabTiles.Add(TileType.CornerNE, new TileConfiguration(TileType.CornerNE, tileCornerSE, new Vector3(0.0f, 270.0f, 0.0f)));
+        prefabTiles.Add(TileType.CornerNW, new TileConfiguration(TileType.CornerNW, tileCornerSE, new Vector3(0.0f, 180.0f, 0.0f)));
+    }
+
+    // given a set of tileTypes, generate a path
+    void GenerateNewPathFromTileTypes(TileType[] tileTypeList)
+    {
+        foreach(TileType currentTileType in tileTypeList)
+        {
+            currentPath.AddTile(new Tile(currentTileType));
+        }
+    }
+
+    // load the path in "currentPath" by using the available prefabs
+    void LoadTrack(Vector3 origin)
+    {
+        // last tile (used to add the new one)
+        Vector3 currentPosition = origin;
+        Direction comingFrom = Direction.South; // the game starts as if the runner came from the south
+        foreach( Tile currentTile in currentPath)
+        {
+            // load the prefab according to the tile
+            TileConfiguration currentTileConfig = prefabTiles[currentTile.tileType];
+            GameObject toInstantiate = currentTileConfig.tilePrefab;
+            GameObject instance = Instantiate(toInstantiate, currentPosition, Quaternion.identity) as GameObject;
+            instance.transform.SetParent(boardHolder.transform); // just so the hierarchy shows it nicely
+            instance.transform.Rotate(currentTileConfig.tileRotation);
+            // calculate next position
+            currentPosition = CalculateNextPosition(currentPosition, currentTileConfig.tileType, 10.0f, comingFrom); // 10.0f since the tiles are square with side 1
+            comingFrom = CalculateNextDirection(comingFrom, currentTileConfig.tileType);
+        }
+    }
+    // calculates the position for the next tile
+    Vector3 CalculateNextPosition(Vector3 currentPosition, TileType currentTileType, float distance, Direction direction)
+    {
+        Vector3 newPosition = new Vector3(currentPosition.x, currentPosition.y, currentPosition.z);
+        switch (currentTileType)
+        {
+            case TileType.Vertical: // either we are coming from the south or the north
+                if(direction == Direction.North) newPosition.z = newPosition.z - distance;
+                else newPosition.z = newPosition.z + distance;
+                break;
+            case TileType.Horizontal:
+                if(direction == Direction.East) newPosition.x = newPosition.x - distance;
+                else newPosition.x = newPosition.x + distance;
+                break;
+            case TileType.CornerSE:
+                if(direction == Direction.South) newPosition.x = newPosition.x + distance; // towards east
+                else newPosition.z = newPosition.z - distance; // towards south
+                break;
+            case TileType.CornerSW:
+                if(direction == Direction.South) newPosition.x = newPosition.x - distance; // towards west
+                else newPosition.z = newPosition.z - distance; // towards south
+                break;
+            case TileType.CornerNE:
+                if(direction == Direction.North) newPosition.x = newPosition.x + distance; // towards east
+                else newPosition.z = newPosition.z + distance; // towards north
+                break;
+            case TileType.CornerNW:
+                if(direction == Direction.North) newPosition.x = newPosition.x - distance; // towards west
+                else newPosition.z = newPosition.z + distance; // towards north
+                break;
+            default:
+                break;
+        }
+        return newPosition;
+    }
+    // calculates the new direction the runner will be after applying the tile (direction for the new position!)
+    Direction CalculateNextDirection(Direction direction, TileType currentTileType)
+    {
+        Direction newDirection = direction;
+        switch (currentTileType)
+        {
+            case TileType.Vertical:
+            case TileType.Horizontal: // direction doesn't change since the tiles go straight
+                break;
+            case TileType.CornerSE: // coming from either south or east
+                if(direction == Direction.South) newDirection = Direction.West;
+                else newDirection = Direction.North;
+                break;
+            case TileType.CornerSW: // from south or west
+                if(direction == Direction.South) newDirection = Direction.East;
+                else newDirection = Direction.North;
+                break;
+            case TileType.CornerNE:
+                if(direction == Direction.North) newDirection = Direction.West;
+                else newDirection = Direction.South;
+                break;
+            case TileType.CornerNW:
+                if(direction == Direction.North) newDirection = Direction.East;
+                else newDirection = Direction.South;
+                break;
+            default:
+                break;
+        }
+        return newDirection;
+    }
+
+    // to be able to save the path in file
+    public void SetPath(Path newPath)
+    {
+        currentPath = newPath;
+    }
+    public Path GetPath()
+    {
+        return currentPath;
+    }
+    
+    // Use this for initialization
+    // TODO: needed?
+    void Start () {
+        
+    }
+    
+    // Update is called once per frame
+    // TODO: needed?
+    void Update () {
+        
+    }
+
+}
+
+// holds a path to be ran. is an abstraction layer to a sorted List
+// TODO check if path needs to be serializable (since it's stored)
+[Serializable]
+public class Path {
+
+    // list with the tiles forming the path in order
+    private ArrayList tileList;
+
+    public Path()
+    {
+        tileList = new ArrayList();
+    }
+
+    public void AddTile(Tile newTile) 
+    {
+        tileList.Add(newTile);
+    }
+
+    // get an enumerator of the tiles
+    public IEnumerator GetEnumerator()
+    {
+        return tileList.GetEnumerator();
+    }
+
+    public int Count() {
+        return tileList.Count;
+    }
+}
+
+[Serializable]
+public class Tile {
+
+    public TileType tileType;
+
+    // given a tile type set the Tile
+    public Tile(TileType tileType)
+    {
+        this.tileType = tileType;
+    }
+}
+
+// define tile possiblities
+public enum TileType
+{
+    Vertical, // tile goes straight vertically
+    Horizontal, // same horizontally
+    CornerSE, // tile turns connecting south and east
+    CornerSW, 
+    CornerNE,
+    CornerNW // tile turns connecting north and east
+};
+
+public class TileConfiguration 
+{
+    public TileType tileType;
+    public GameObject tilePrefab;
+    public Vector3 tileRotation;
+
+    public TileConfiguration (TileType tileType, GameObject tilePrefab, Vector3 tileRotation)
+    {
+        this.tileType = tileType;
+        this.tilePrefab = tilePrefab;
+        this.tileRotation = tileRotation;
+    }
+}
+
+enum Direction
+{
+    South,
+    North,
+    East,
+    West
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class GameController : MonoBehaviour
+{
+
+    public static GameController instance = null;
+    private BoardController boardController;
+
+    // Use this for initialization
+    void Awake()
+    {
+        // make sure there is only one instance of GameController
+        if (instance == null)
+        {
+            instance = this;
+        } else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+        // get other necessary components
+        boardController = GetComponent<BoardController>();
+        // initialize the game
+        InitGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    // Initialize the game
+    void InitGame()
+    {
+        boardController.BoardSetup();
+    }
+
+
+    // functions to save and delete game data to a file
+    // TODO test. this function is currently not used
+    public void Save()
+    {
+        // open a binary file
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file;
+        if (File.Exists(Application.persistentDataPath + "/gameStatus.dat"))
+        {
+            file = File.Open(Application.persistentDataPath + "/gameStatus.dat", FileMode.Open);
+        } else
+        {
+            file = File.Create(Application.persistentDataPath + "/gameStatus.dat");
+        }
+        // fill data object and save it
+        GameStatusData gameData = new GameStatusData();
+        gameData.currentPath = boardController.GetPath();
+        bf.Serialize(file, gameData);
+        // close the file
+        file.Close();
+    }
+
+    // TODO test. this function is currently not used
+    public void Load()
+    {
+        // open a binary file
+        if (File.Exists(Application.persistentDataPath + "/gameStatus.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/gameStatus.dat", FileMode.Open);
+            // get data object and save it
+            GameStatusData gameData = (GameStatusData)bf.Deserialize(file);
+            // close the file
+            file.Close();
+            // pass data from data object to the current game
+            boardController.SetPath(gameData.currentPath);
+        }
+    }
+}
+
+// class to contain the status of the game
+[Serializable]
+class GameStatusData
+{
+    // the race map
+    // TODO think: should probably be some other class to just store the data, not a Path directy.
+    public Path currentPath;
+}
+
+
+
